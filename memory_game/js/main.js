982366a6699e310c1066236409788f01ea52c5c6
Suneetha Malkani <suneetha@radar108.com>
2019-09-09 20:26:22 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,24 +4,32 @@ console.log("Up and running!");
 const cards=["queen","queen","king","king"];
 const cardsInPlay=[];
 
-cardOne=cards[0];
-cardsInPlay.push(cardOne);
+function	checkForMatch()
 
-
-console.log("User flipped " + cardOne);
-
-cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-
-if (cardsInPlay.length===2) 
 {
-console.log("Length is 2 " );
+console.log("cardsInPlay length " + cardsInPlay.length);
+
 if (cardsInPlay[0]===cardsInPlay[1])
 {alert("You found a match!")}
 else
 {alert("Sorry! Try again")}
 
-
 }
+
+
+
+function flipCard(cardId)
+{
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+cardOne=cards[0];
+cardsInPlay.push(cardOne);
+flipCard(0);
+flipCard(1);
+flipCard(2);
+flipCard(3);
+
+
